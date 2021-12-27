3dc37a8aecbee32b82ed736d6061720ab59d887a
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2021-12-27 05:11:31 +0500

    cdk app

---
+++ step16_simple_email_service/save-received-mails-in-s3/test/save-received-mails-in-s3.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as SaveReceivedMailsInS3 from '../lib/save-received-mails-in-s3-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/save-received-mails-in-s3-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SaveReceivedMailsInS3.SaveReceivedMailsInS3Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
